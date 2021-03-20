7e2de5b04667cf71f3221551062fce7f277e63d7
EvseevSA <81044108+EvseevSA@users.noreply.github.com>
2021-03-20 23:06:33 +0300

    Update Main.java

--- Lab1/src/com/evseev/Main.java
+++ Lab1/src/com/evseev/Main.java
@@ -3,7 +3,7 @@ package com.evseev;
 public class Main {
 
     public static void main(String[] args) {
-	Primes.getPrimes(null);
+	Primes.getPrimes();
 	System.out.println();
 	Palindrome.identify(new String[]{"madam", "racecar", "apple", "kayak",
             "song", "noon"});
